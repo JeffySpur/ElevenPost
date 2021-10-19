73de3cb9a65cb262d1140f47b4d00c1d03d81078
amyjprogrammer <amyjprogrammer@gmail.com>
2021-10-19 16:47:39 -0400

    added comment.cs and comment create

---
+++ 24HourPost.Models/CommentCreate.cs
@@ -0,0 +1,20 @@
+﻿using _24HourPost.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24HourPost.Models
+{
+    public class CommentCreate
+    {
+        [Required]
+        [MinLength(2, ErrorMessage = "Please enter at least 2 characters.")]
+        [MaxLength(250, ErrorMessage = "Only enter 250 characters.")]
+        public string Text { get; set; }
+
+        public List<Comment> Replies { get; set; }
+    }
+}
