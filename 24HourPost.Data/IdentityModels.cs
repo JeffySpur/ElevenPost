5374aa6ce156bbbcb19fa8d867e73f6f794f9f5d
Jeffrey Spurling <89798840+JeffySpur@users.noreply.github.com>
2021-10-19 17:15:12 -0400

    post

--- 24HourPost.Data/IdentityModels.cs
+++ 24HourPost.Data/IdentityModels.cs
@@ -2,6 +2,7 @@
 using System.Data.Entity.ModelConfiguration;
 using System.Security.Claims;
 using System.Threading.Tasks;
+using _24HourPost.Data;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.AspNet.Identity.Owin;
