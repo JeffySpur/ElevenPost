c15755e365dd0e49e095da4a385dc256f4caf291
amyjprogrammer <amyjprogrammer@gmail.com>
2021-10-19 16:47:39 -0400

    added comment.cs and comment create

---
+++ 24HourPost.Models/CommentListItem.cs
@@ -0,0 +1,21 @@
+﻿using _24HourPost.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24HourPost.Models
+{
+    public class CommentListItem
+    {
+        [Display(Name = "Comment Id")]
+        public int CommentId { get; set; }
+
+        public string Text { get; set; }
+
+        public List<Comment> Replies { get; set; }
+
+    }
+}
