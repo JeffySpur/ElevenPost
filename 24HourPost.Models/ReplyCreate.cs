b0beb814842c1ba8f8942b62f1d3ce8f3a0dede9
Taylor <taylor.randol@gmail.com>
2021-10-20 09:50:18 -0400

    Replys Added

---
+++ 24HourPost.Models/ReplyCreate.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24HourPost.Models
+{
+    public class ReplyCreate
+    {
+        [Required]
+        [MinLength(2, ErrorMessage = "Please enter at least 2 characters.")]
+        [MaxLength(250, ErrorMessage = "Only enter 250 characters.")]
+        public string Text { get; set; }
+        public int CommentId { get; set; }
+    }
+}
---
+++ 24HourPost.Models/ReplyListItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24HourPost.Models
+{
+    public class ReplyListItem
+    {
+        [Display(Name = "Reply Id")]
+        public int ReplyId { get; set; }
+
+        public string Text { get; set; }
+    }
+}
